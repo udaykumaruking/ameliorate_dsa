844bb021ea780235e79cf58902a92cc454bf96ba
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-06-06 16:14:37 +0530

    Card done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +13,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getElevation(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return 15.toDouble();
+      }
+      return 5.toDouble();
+    }
+
     int totquest = 30;
     return MaterialApp(
       home: Scaffold(
@@ -37,7 +51,7 @@ class MyApp extends StatelessWidget {
                   style: GoogleFonts.lato(
                     textStyle: TextStyle(
                       fontSize: 25,
-                      letterSpacing: 1.7,
+                      letterSpacing: 1.2,
                       fontWeight: FontWeight.w500,
                       color: Colors.black,
                     ),
@@ -57,6 +71,78 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                 ),
+                BuildSizedBox(
+                  height: 50,
+                ),
+                Wrap(
+                  children: [
+                    Container(
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: kVoilet,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              'Array',
+                              style: GoogleFonts.jost(
+                                  textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1.5,
+                              )),
+                            ),
+                            BuildSizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Total Questions: 40',
+                              style: GoogleFonts.montserrat(
+                                  textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 1,
+                              )),
+                            ),
+                            BuildSizedBox(
+                              height: 15,
+                            ),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Color.fromRGBO(101, 83, 178, 1)),
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  elevation: MaterialStateProperty.all(0)),
+                              onPressed: () {},
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                child: Text(
+                                  'Start Now',
+                                  style: GoogleFonts.montserrat(
+                                      textStyle: TextStyle(
+                                    fontSize: 16,
+                                    letterSpacing: 0.8,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.white,
+                                  )),
+                                ),
+                              ),
+                            ),
+                            Text(''),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
